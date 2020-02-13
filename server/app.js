58f3ee51ea0fae893507dfd22528078acf93deec
Asten Fuller <astenhfuller@gmail.com>
2020-02-13 14:00:20 -0800

    seems to work, tests not passing

--- server/app.js
+++ server/app.js
@@ -11,14 +11,38 @@ app.use(morgan('dev'));
 let cache = [];
 
 app.get('/', (req, res) => {
-    axios({
-        url: `http://omdbapi.com/?i=${req.query.i}&apikey=${process.env.OMDB_API_KEY}`,
-        method: 'get'
-    })
-    .then((response) => {
-        res.send(response.data);
-    })
-    .catch((err) => console.log(err))
+    if(req.query.i) {
+        if(cache[req.query.i]) {
+            res.send(cache[req.query.i])
+            console.log('being sent from cache')
+        } else {
+            axios({
+                url: `http://omdbapi.com/?i=${req.query.i}&apikey=${process.env.OMDB_API_KEY}`,
+                method: 'get'
+            })
+            .then((response) => {
+                cache[response.data.imdbID] = response.data;
+                res.send(response.data);
+            })
+            .catch((err) => console.log(err))
+        }
+    }
+    else if (req.query.t) {
+        if(cache[req.query.t.toLowerCase()]) {
+            res.send(cache[req.query.t.toLowerCase()])
+            console.log('being sent from cache')
+        } else {
+            axios({
+                url: `http://omdbapi.com/?t=${req.query.t}&apikey=${process.env.OMDB_API_KEY}`,
+                method: 'get'
+            })
+            .then((response) => {
+                cache[response.data.Title.toLowerCase()] = response.data;
+                res.send(response.data);
+            })
+            .catch((err) => console.log(err))
+        }
+    }
 });
 
 
